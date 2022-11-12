78e990d3f752253fa909b9e8bb939fb86e8fcbe1
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-12 08:11:00 +0100

    Add project files.

---
+++ DateTimeT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateTimeT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2022, 11, 12);
+        }
+    }
+}
